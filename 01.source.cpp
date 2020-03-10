e61d39784c84c5322b5978e1234077274d76e66d
Code Hunger <beinghavingbreackfast@gmail.com>
2020-03-10 22:19:35 +0200

    The first three weeks

---
+++ 01.source.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+
+struct Point
+{
+	int x;
+	int y;
+
+	// Structs can have functions *inside* them! We call them "methods".
+	// Look in the main function for example uses.
+	void print()
+	{
+		// @Reminder: the type of `this` is `Point* const`. In other words, an
+		// immutable (const) pointer to a mutable (non-const) Point instance.
+
+		// @Reminder: all variables (member fields) of the object on which this
+		// method is called (point.print()) are available like local variables
+		// here.
+		// Another way to write `x` is `this->x` or `(*this).x`.
+		cout << "(" << x << ";" << y << ")";
+	}
+};
+
+struct Circle
+{
+	// struct inside struct - no problem!
+	// Benefits: we can reuse Point's functionality. E.g. we can call
+	// `p.print()` instead of writing manually `cout << x << ' ' << y;`.
+	Point p;
+	double r;
+
+	void print()
+	{
+		cout << r << " ";
+		p.print();
+		cout << endl;
+	}
+};
+
+struct Rectangle
+{
+	Point p;
+	double width;
+	double height;
+
+	void print()
+	{
+		cout << width << " " << height << " ";
+		p.print();
+		cout << endl;
+	}
+};
+
+struct Window
+{
+	// Arrays of structs inside struct!
+	Circle circles[2];
+	Rectangle rectangles[2];
+
+	void print()
+	{
+		// NOTE: funny syntax.
+		// @TODO: Understand how it works by the end of the semester!
+		for (Circle& c : circles) {
+			c.print();
+		}
+
+		for (Rectangle& r : rectangles) {
+			r.print();
+		}
+	}
+};
+
+int main()
+{
+	// Curly braces give initial values to structs.
+	Point p{5, 10};
+	Rectangle r{3, 4, p};
+	Circle c{6, p}; //{6,{5,10}}
+
+	// Method calls! Note the special syntax.
+	p.print(); // The `this` inside the print() function equals `&p`.
+	c.print();
+
+	// More complex curly braces!
+	Window w1{{c, c}, {r, r}};
+	Window w2{{c, c}, {r, r}};
+
+	w1.print();
+	cout << endl;
+
+	w2.print();
+	return 0;
+}
---
+++ 02.source.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+using namespace std;
+
+struct Point
+{
+	int x;
+	int y;
+
+	// Structs can have functions *inside* them! We call them "methods".
+	// Look in the main function for example uses.
+	void print()
+	{
+		// @Reminder: the type of `this` is `Point* const`. In other words, an
+		// immutable (const) pointer to a mutable (non-const) Point instance.
+
+		// @Reminder: all variables (member fields) of the object on which this
+		// method is called (point.print()) are available like local variables
+		// here.
+		// Another way to write `x` is `this->x` or `(*this).x`.
+		cout << "(" << x << ";" << y << ")";
+	}
+};
+
+struct Circle
+{
+	// struct inside struct - no problem!
+	// Benefits: we can reuse Point's functionality. E.g. we can call
+	// `p.print()` instead of writing manually `cout << x << ' ' << y;`.
+	Point p;
+	double r;
+
+	void print()
+	{
+		cout << r << " ";
+		p.print();
+		cout << endl;
+	}
+};
+
+struct Rectangle
+{
+	Point p;
+	double width;
+	double height;
+
+	void print()
+	{
+		cout << width << " " << height << " ";
+		p.print();
+		cout << endl;
+	}
+};
+
+struct Window
+{
+	Circle* circles;
+	Rectangle* rectangles;
+
+	void print()
+	{
+		// @TODO Doesn't work! You'll see why. For now, just fix it.
+
+		// for(Circle & c: circles) {
+		//     c.print();
+		// }
+		// for(Rectangle & r: rectangles) {
+		//     r.print();
+		// }
+	}
+};
+
+struct DynamicArray
+{
+	Circle* circles;
+
+	/*
+	 * Constructor - a special function, automatically called on object
+	 * initialization.
+	 * @TODO: check what happens if you pass n = 0, i.e. what does `new T[0]`
+	 * do. Look it up on the internet and explain what you observed.
+	 *
+	 * @TODO. Proper initialization of the member fields
+	 */
+	DynamicArray(unsigned n)
+	{
+		array = new Circle[n];
+		cout << "Dynamic array allocated #" << (countt++) << endl;
+	}
+
+	/*
+	 * Destructor - a special function, automatically called when the object
+	 * leaves scope (the set of curly braces it was defiend within).
+	 *
+	 * An easy way not to forget to free whatever you allocated!
+	 */
+	~DynamicArray()
+	{
+		delete[] array;
+		cout << "~DynamicArray(): memory cleared." << endl;
+	}
+};
+
+int main()
+{
+	Point p{5, 10};
+	Rectangle r{3, 4, p};
+	Circle c{6, p}; // We can also nest curly braces: {6, {5,10}}
+
+	// Method calls! Note the special syntax.
+	p.print(); // The `this` inside the print() function equals `&p`.
+	c.print();
+
+	unsigned n;
+	cin >> n;
+
+	// NOTE: We call the constructor here with an argument 'n':
+	DynamicArray circles{n};
+
+	// Compare the old initialization below ↓ with the new above ↑. Admit it
+	// looks better. And remember it also lets us (almost) forget about delete.
+	Rectangle* rectangles = new Rectangle[n];
+	delete rectangles;
+
+	// Window w1{circles, rectangles};
+	// Window w2{circles, rectangles};
+	// w1.print();
+	// cout << endl;
+	// w2.print();
+	return 0;
+}
