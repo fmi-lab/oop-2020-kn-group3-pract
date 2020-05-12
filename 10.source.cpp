fc2a26b8803cafe87de0f2f47cfc7d15a23ec8f2
Hakan Hasan <ninties@abv.bg>
2020-05-12 13:51:14 +0300

    fix access modifiers of evalAt and make TODOs in week 10 source

--- 10.source.cpp
+++ 10.source.cpp
@@ -37,8 +37,30 @@ struct Positives : Set
 	Positives* clone() const override { return new Positives(); }
 };
 
-// @TODO Define the sets of positive, nonnegative numbers and sets of intervals
-// of the form (a, b)
+struct NonNegatives : Set
+{
+	bool check(float x) const override { return x >= 0; }
+
+	NonNegatives* clone() const override { return new NonNegatives(); }
+};
+
+struct Interval : Set
+{
+	const float a, b;
+
+	Interval(const float a, const float b) : a(a), b(b) {}
+
+	bool check(float x) const override { return a < x && b > x; }
+
+	Interval* clone() const override { return new Interval(*this); }
+};
+
+struct TanDomain : Set
+{
+	bool check(float x) const override { return ceilf(x / (M_PI / 2)) != floorf(x / (M_PI / 2)) ||  (int)(x / (M_PI / 2)) % 2 == 0; }
+
+	TanDomain* clone() const override { return new TanDomain(); }
+};
 
 class RealFunc
 {
@@ -75,6 +97,7 @@ struct Exponent : RealFunc
 {
 	Exponent() : RealFunc{AllReals{}, "exp"} {}
 
+protected:
 	virtual float evalAt(float x) override { return std::pow(E, x); }
 };
 
@@ -82,6 +105,7 @@ struct Log : RealFunc
 {
 	Log() : RealFunc{Positives{}, "log"} {}
 
+protected:
 	float evalAt(float x) override { return std::log(x); }
 };
 
@@ -90,6 +114,7 @@ struct BoyanFunction : RealFunc
 	int n;
 	int* myBigArr = new int[100]; // Illustration of the need of virtual d-tors
 
+protected:
 	float evalAt(float x) override
 	{
 		n++;
@@ -110,6 +135,7 @@ struct Compose : RealFunc
 	{
 	}
 
+protected:
 	float evalAt(float x) override
 	{
 		return f->safeEval(g->safeEval(x));
@@ -120,19 +146,27 @@ struct Sin : RealFunc
 {
 	Sin() : RealFunc{AllReals{}, "sin"} {}
 
+protected:
 	float evalAt(float x) override { return std::sin(x); }
 };
 
-// @TODO Define `struct Tan` and carefully construct its domain Set.
+struct Tan : RealFunc
+{
+	Tan() : RealFunc{TanDomain{}, "tan"} {}
 
-// @TODO Define `struct Pow` which raises `x` to a power, specified as a
-// constructor argument.
+protected:
+	float evalAt(float x) override { return std::tan(x); }
+};
 
-// @TODO Define `struct Sum` which represents the sum of all functions in a
-// given array.
+struct Pow : RealFunc
+{
+	const float exp;
 
-// @TODO Define global operators `*`, `+`, and `>>` which respectively act as
-// multiplication, addition and composition of functions.
+	Pow(const float exp) : RealFunc{Positives{}, "pow"}, exp(exp) {}
+
+protected:
+	float evalAt(float x) override { return std::pow(x, exp); }
+};
 
 struct Sum : public RealFunc
 {
@@ -143,10 +177,28 @@ struct Sum : public RealFunc
 	{
 	}
 
+protected:
 	float evalAt(float x) override { return f->safeEval(x) + g->safeEval(x); }
 };
 
+struct Product : public RealFunc
+{
+	RealFunc *f, *g;
+
+	Product(RealFunc* f, RealFunc* g)
+	    : RealFunc{Intersect{f->domain, g->domain}, "Product"}, f(f), g(g)
+	{
+	}
+
+protected:
+	float evalAt(float x) override { return f->safeEval(x) * g->safeEval(x); }
+};
+
+Product operator*(RealFunc& a, RealFunc& b) { return {&a, &b}; }
+
 Sum operator+(RealFunc& a, RealFunc& b) { return {&a, &b}; }
+
+Compose operator>>(RealFunc& a, RealFunc& b) { return {&a, &b}; }
 
 int main()
 {
@@ -160,4 +212,9 @@ int main()
 
 	RealFunc* base = &comp;
 	std::cout << base->safeEval(3) << std::endl;
+
+	// problem with Compose's defined domain - example
+	Compose comp2{&log, &sin};
+	base = &comp2;
+	std::cout << base->safeEval(3 * M_PI / 2) << std::endl;
 }
